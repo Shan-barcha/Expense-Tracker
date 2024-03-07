789471228c085688dd36dc454a58b4809b3a29f9
Shan-barcha <shanbarcha11@gmail.com>
2024-03-07 20:22:05 +0500

    Feat:adding transactions and total balance

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpense />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
-import Header from "./components/Header.vue";
-import Balance from "./components/Balance.vue";
-import IncomeExpense from "./components/IncomeExpense.vue";
-import TransactionList from "./components/TransactionList.vue";
-import AddTransaction from "./components/AddTransaction.vue";
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import { ref, computed } from 'vue';
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction,
-  },
-};
+const transactions = ref([
+  { id: 1, text: 'flower', amount: -19.9 },
+  { id: 1, text: 'salary', amount: 4009.9 },
+  { id: 1, text: 'Camera', amount: -200.9 },
+  { id: 1, text: 'Books', amount: 299.9 },
+]);
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// export default {
+//   components: {
+//     Header,
+//     Balance,
+//     IncomeExpense,
+//     TransactionList,
+//     AddTransaction,
+//   },
+// };
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,77 @@
-<template >
-   <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-          Cash <span>$400</span><button class="delete-btn">x</button>
-        </li>
-      </ul> 
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<!-- <script setup>
+  const transactions = [
+      {id:1, text:"flower", amount:-19.90},
+      {id:1, text:"salary", amount:4009.90},
+      {id:1, text:"Camera", amount:-200.90},
+      {id:1, text:"Books", amount:99.90},
+
+    ];
+
+</setup> -->
+
+<!-- <script>
+// options API
+// export default{
+//   data(){
+//     return{
+//     transactions:[
+//       {id:1, text:"flower", amount:-19.90},
+//       {id:1, text:"salary", amount:4009.90},
+//       {id:1, text:"Camera", amount:-200.90},
+//       {id:1, text:"Books", amount:99.90},
+
+//     ],
+//   };
+//   },
+// };
+// composition API
+
+// two way to use composition API
+
+// export default{
+// setup() {
+//   const transactions= [
+//       {id:1, text:"flower", amount:-19.90},
+//       {id:1, text:"salary", amount:4009.90},
+//       {id:1, text:"Camera", amount:-200.90},
+//       {id:1, text:"Books", amount:99.90},
+
+//     ],
+// if we want to create a function
+
+// myfunction (){
+//   what ever we want to do
+// }
+//     return{
+//       transactions,
+//  myfunction
+
+//     };
+//   },
+// }
+
+//</script> -->
